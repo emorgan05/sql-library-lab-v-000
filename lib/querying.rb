7838818b4fc6d8c7310baef4cb56221d141f0a9b
Emily Morgan <emorgan05@gmail.com>
2017-10-22 20:17:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,6 +27,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters ON characters.series_id = series.id
+  WHERE characters.species = 'human'
   ORDER BY COUNT(characters.species) DESC
   LIMIT 1;"
 end
