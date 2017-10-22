1b14b64db46f47bbb9ee128d9dca28d5b0bf21c2
Emily Morgan <emorgan05@gmail.com>
2017-10-22 19:28:29 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES
+("Harry Potter", 1, 1), 
+("The Kingkiller Chronicles", 2, 2);
+
+INSERT INTO subgenres (name) VALUES 
+("Modern"), 
+("Faerie");
+
+INSERT INTO authors (name) VALUES
+("JK Rowling"),
+("Patrick Rothfuss");
+
+INSERT INTO books (title, year, series_id) VALUES
+("Harry Potter and the Sorcerer's Stone", 1998, 1),
+("Harry Potter and the Chamber of Secrets", 1999, 1),
+("Harry Potter and the Prisoner of Azkabahn", 2001, 1),
+("The Name of the Wind", 2007, 2),
+("The Wise Man's Fear", 2011, 2),
+("The Doors of Stone", 2018, 2);
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
+()
